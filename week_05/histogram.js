c8492c3b0a021011777550a6f55040ae2c194c36
RConnie <rebecca.connie@gmail.com>
2022-10-27 16:34:56 -0500

    updated class examples

--- week_05/histogram.js
+++ week_05/histogram.js
@@ -3,9 +3,9 @@
 const height = 400,
     width = 600,
     margin = ({ top: 25, right: 10, bottom: 50, left: 10 }),
-    padding = 1;
+    padding = 1;//between histogram bars
 
-const svg = d3.select("#chart")
+const svg = d3.select("#chart")//create svg and appending to html
     .append("svg")
     .attr("viewBox", [0, 0, width, height]);
 
@@ -16,36 +16,44 @@ d3.json('climate-jan.json').then((data) => {
     .range([margin.left, width - margin.right]);
   
   const y = d3.scaleLinear()
-    .range([height - margin.bottom, margin.top])
-    .domain([0,10]);
+    .domain([0,10]) //manually putting in data
+    .range([height - margin.bottom, margin.top]);
     
   svg.append("g")
     .attr("transform", `translate(0,${height - margin.bottom + 5})`)
     .call(d3.axisBottom(x));
 
-  const binGroups = svg.append("g")
+  const binGroups = svg.append("g") //histogram bins
     .attr("class", "bin-group");
 
   const bins = d3.bin()
-    .thresholds(10)
-    .value(d => d.average)(data);
+    .thresholds(10) //approx # of bars who want data across
+    .value(d => d.average)(data); //data you want to use
 
-  let g = binGroups.selectAll("g")
+  let g = binGroups.selectAll("g") //setting up group of data that comes out of bin
     .data(bins)
     .join("g");
 
-  // g.append("rect")
-  //   .attr("x", d => x(d.x0) + (padding / 2))
-  //   .attr("y", d => y(d.length))
-  //   .attr("width", d => x(d.x1) - x(d.x0) - padding)
-  //   .attr("height", d => height - margin.bottom - y(d.length))
-  //   .attr("fill", "steelblue");
+  g.append("rect") //appending rectangles 
+    .attr("x", d => x(d.x0) + (padding / 2))
+    .attr("width", d => x(d.x1) - x(d.x0) - padding)
+    .attr("fill", "steelblue")
+    .attr("height", 0) //havign bars grow, this is setign starting position
+    .attr("y", height - margin.bottom)
+    .transition()//will have bars grow
+    .duration(750)
+    .attr("y", d => y(d.length))
+    .attr("height", d => height - margin.bottom - y(d.length)); //end posiiton
 
-  // g.append("text")
-  //   .text(d => d.length)
-  //   .attr("x", d => x(d.x0) + (x(d.x1) - x(d.x0)) / 2)
-  //   .attr("y", d => y(d.length) - 5)
-  //   .attr("text-anchor", "middle")
-  //   .attr("fill", "#333");
+  g.append("text")
+    .text(d => d.length)
+    .attr("x", d => x(d.x0) + (x(d.x1) - x(d.x0)) / 2)
+    .attr("y", d => y(d.length) - 5)
+    .attr("text-anchor", "middle")
+    .attr("fill", "#333")
+    .attr("y", height - margin.bottom)
+    .transition()
+    .duration(750)
+    .attr("y");//find this code on git later
 
 });
--- week_05/scatterplot.js
+++ week_05/scatterplot.js
@@ -1,61 +1,62 @@
 let height = 400,
     width = 600,
-    margin = ({ top: 25, right: 30, bottom: 35, left: 40 });
+    margin = ({ top: 25, right: 30, bottom: 35, left: 40 }); 
   
 const svg = d3.select("#chart")
     .append("svg")
-    .attr("viewBox", [0, 0, width, height]);
+    .attr("viewBox", [0, 0, width, height]); //putting sgv element into chart(linking on html), 
+    //viewbox- changes size based on screen size
 
-d3.csv('penguins.csv').then(data => {
+d3.csv('penguins.csv').then(data => { //linking data
   
-  let x = d3.scaleLinear()
-    .domain(d3.extent(data, d => d.body_mass_g)).nice()
-    .range([margin.left, width - margin.right]);
+  let x = d3.scaleLinear() // x scale
+    .domain(d3.extent(data, d => d.body_mass_g)).nice() //data
+    .range([margin.left, width - margin.right]); //space data takes up
 
-  let y = d3.scaleLinear()
-    .domain(d3.extent(data, d => d.flipper_length_mm)).nice()
-    .range([height - margin.bottom, margin.top]);
+  let y = d3.scaleLinear() 
+    .domain(d3.extent(data, d => d.flipper_length_mm)).nice() // data, take min and max value, no zero on y axis
+    .range([height - margin.bottom, margin.top]); 
 
   svg.append("g")
-    .attr("transform", `translate(0,${height - margin.bottom})`)
+    .attr("transform", `translate(0,${height - margin.bottom})`) //drawing the x axis
     .attr("class", "x-axis")
     .call(d3.axisBottom(x).tickFormat(d => (d/1000) + "kg").tickSize(-height + margin.top + margin.bottom))
 
   svg.append("g")
-    .attr("transform", `translate(${margin.left},0)`)
+    .attr("transform", `translate(${margin.left},0)`) //drawing y axis
     .attr("class", "y-axis")
     .call(d3.axisLeft(y).tickSize(-width + margin.left + margin.right))
 
-//   svg.append("g")
-//     .attr("fill", "black")
-//     .selectAll("circle")
-//     .data(data)
-//     .join("circle")
-//     .attr("cx", d => x(d.body_mass_g))
-//     .attr("cy", d => y(d.flipper_length_mm))
-//     .attr("r", 2)
-//     .attr("opacity", 0.75);
+  svg.append("g") //addign group element, then joining data of circles
+    .attr("fill", "black")//create circles
+    .selectAll("circle")
+    .data(data)
+    .join("circle") //join circles
+    .attr("cx", d => x(d.body_mass_g)) 
+    .attr("cy", d => y(d.flipper_length_mm))
+    .attr("r", 2) //r is radius
+    .attr("opacity", 0.75);
 
-//   const tooltip = d3.select("body").append("div")
-//     .attr("class", "svg-tooltip")
-//     .style("position", "absolute")
-//     .style("visibility", "hidden");
+  const tooltip = d3.select("body").append("div") //create a tool tip, select html body, appending a div,will put data into the div
+    .attr("class", "svg-tooltip") //from styles.css ln 18-23
+    .style("position", "absolute")
+    .style("visibility", "hidden");
 
-//   d3.selectAll("circle")
-//     .on("mouseover", function(event, d) {
-//       d3.select(this).attr("fill", "red");
-//       tooltip
-//         .style("visibility", "visible")
-//         .html(`Species: ${d.species}<br />Island: ${d.island}<br />Weight: ${d.body_mass_g/1000}kg`);
-//     })
-//     .on("mousemove", function(event) {
-//       tooltip
-//         .style("top", (event.pageY - 10) + "px")
-//         .style("left", (event.pageX + 10) + "px");
-//     })
-//     .on("mouseout", function() {
-//       d3.select(this).attr("fill", "black");
-//       tooltip.style("visibility", "hidden");
-//     })
+  d3.selectAll("circle")
+    .on("mouseover", function(event, d) { //creating an event
+      d3.select(this).attr("fill", "red"); //fill red when selected
+      tooltip
+        .style("visibility", "visible") //originally hidden, now make it visible
+        .html(`Species: ${d.species}<br />Island: ${d.island}<br />Weight: ${d.body_mass_g/1000}kg`); //passing data info into html
+    })
+    .on("mousemove", function(event) {//when mouse moves capture x and y event(event listener)
+      tooltip
+        .style("top", (event.pageY - 10) + "px")//make tool tip close to 
+        .style("left", (event.pageX + 10) + "px");
+    })
+    .on("mouseout", function() { //allow to turn tool tip text and red color on and off
+      d3.select(this).attr("fill", "black");
+      tooltip.style("visibility", "hidden");
+    })
     
 });
